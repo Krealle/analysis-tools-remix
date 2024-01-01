8bad76885f8afa078e7ffeb0b89297889665cfef
Vollmer <kristianvollmer@gmail.com>
2024-01-01 11:07:38 +0100

    Update ESLint configuration and add lint:fix script

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,90 +1,90 @@
-/**
- * This is intended to be a basic starting point for linting in your app.
- * It relies on recommended configs out of the box for simplicity, but you can
- * and should modify this configuration to best suit your team's needs.
- */
-
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:node/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
+    project: true,
     ecmaVersion: "latest",
     sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
   },
+  plugins: ["react", "jsx-a11y", "@typescript-eslint", "import"],
   env: {
     browser: true,
     commonjs: true,
     es6: true,
+    node: true,
   },
-
-  // Base config
-  extends: ["eslint:recommended", "plugin:node/recommended"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+    formComponents: ["Form"],
+    linkComponents: [
+      { name: "Link", linkAttribute: "to" },
+      { name: "NavLink", linkAttribute: "to" },
+    ],
+    node: {
+      resolvePaths: ["node_modules/@types"],
+      tryExtensions: [".js", ".json", ".node", ".ts", ".d.ts", ".tsx"],
+    },
+    "import/internal-regex": "^~/",
+    "import/resolver": {
+      node: {
+        extensions: [".ts", ".tsx"],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  ignorePatterns: ["*.cjs"],
 
   rules: {
     eqeqeq: ["error", "always"],
+    "no-plusplus": "error",
+    "one-var": [
+      "error",
+      {
+        initialized: "never",
+      },
+    ],
+    "no-implicit-coercion": "error",
+
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-unsafe-enum-comparison": "warn",
+
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "@typescript-eslint/prefer-as-const": "error",
+    "@typescript-eslint/await-thenable": "error",
+
+    "react/jsx-curly-brace-presence": [
+      "warn",
+      { props: "never", children: "never" },
+    ],
+    "react/jsx-boolean-value": ["warn", "never"],
   },
 
   overrides: [
-    // React
     {
-      files: ["**/*.{js,jsx,ts,tsx}"],
-      plugins: ["react", "jsx-a11y"],
-      extends: [
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        "plugin:react-hooks/recommended",
-        "plugin:jsx-a11y/recommended",
-      ],
-      settings: {
-        react: {
-          version: "detect",
-        },
-        formComponents: ["Form"],
-        linkComponents: [
-          { name: "Link", linkAttribute: "to" },
-          { name: "NavLink", linkAttribute: "to" },
-        ],
-      },
-    },
-
-    // Typescript
-    {
-      files: ["**/*.{ts,tsx}"],
-      plugins: ["@typescript-eslint", "import"],
-      parser: "@typescript-eslint/parser",
-      settings: {
-        "import/internal-regex": "^~/",
-        "import/resolver": {
-          node: {
-            extensions: [".ts", ".tsx"],
-          },
-          typescript: {
-            alwaysTryTypes: true,
-          },
-        },
-      },
+      /** Functions in ZustandStore return void, no need to type assert  */
+      files: ["./app/zustand/*.ts"],
       rules: {
-        "@typescript-eslint/no-unused-vars": "warn",
-        "@typescript-eslint/no-explicit-any": "warn",
-        "@typescript-eslint/explicit-module-boundary-types": "warn",
-        "node/no-missing-import": "off",
-        "@typescript-eslint/explicit-function-return-type": "warn",
-      },
-      extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/recommended",
-        "plugin:import/typescript",
-      ],
-    },
-
-    // Node
-    {
-      files: [".eslintrc.js"],
-      env: {
-        node: true,
+        "@typescript-eslint/explicit-function-return-type": "off",
       },
     },
   ],
