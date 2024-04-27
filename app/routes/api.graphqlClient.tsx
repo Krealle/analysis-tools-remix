ea7f3907353001fadba1f430c36abf6d587ba889
Vollmer <kristianvollmer@gmail.com>
2024-04-27 16:43:23 +0200

    allow server side parse of session

--- app/routes/api.graphqlClient.tsx
+++ app/routes/api.graphqlClient.tsx
@@ -1,29 +1,15 @@
-import { /* HeadersFunction, */ LoaderFunction, json } from "@remix-run/node";
+import { LoaderFunction, json } from "@remix-run/node";
 import { GraphQLClient } from "graphql-request";
 import { Queries, QueryTypes } from "../wcl/types/graphql/queries";
 import { Variables } from "../wcl/util/queryWCL";
 import { AccessSession, getSession } from "./sessions";
-
-/** Cache for storing reports, to not have to re-fetch data after a refresh */
-/* const CACHE = new Map<string, unknown>(); */
-
-/* const cacheControl = "Cache-Control";
-
-export const headers: HeadersFunction = () => {
-  const headers: HeadersInit = {};
-
-  headers[cacheControl] = `public, s-maxage=100`;
-  headers["CDN-Cache-Control"] = `public, s-maxage=60`;
-  headers["Vercel-CDN-Cache-Control"] = `public, s-maxage=300`;
-
-  return headers;
-}; */
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
   const requestType = url.searchParams.get("requestType") as keyof QueryTypes;
   const variables = url.searchParams.get("variables");
   const session = await getSession(request.headers.get("Cookie"));
+  const maybeAccessSession = url.searchParams.get("session");
 
   if (!requestType) {
     return json({ error: `Missing request type` });
@@ -35,18 +21,20 @@ export const loader: LoaderFunction = async ({ request }) => {
     return json({ error: `Invalid request type` });
   }
 
-  /* const cacheKey = `${requestType}-${variables}`;
-  const cachedData = CACHE.get(cacheKey); */
-  /** Make sure we allow for fetching new fights */
-  /* if (cachedData && requestType !== "getWCLReportQuery") {
-    return cachedData;
-  } */
-
   const parsedVariables = JSON.parse(variables) as Variables;
 
-  const accessSession = session.get("accToken") as AccessSession;
+  let accessSession = session.get("accToken") as AccessSession;
+
+  if (!accessSession) {
+    if (!maybeAccessSession) {
+      return json({ error: `Missing authorization` });
+    }
+
+    const parsedMaybeSession = JSON.parse(maybeAccessSession) as AccessSession;
+    accessSession = parsedMaybeSession;
+  }
+
   if (
-    !accessSession ||
     !accessSession.expirationTime ||
     accessSession.expirationTime < Math.floor(Date.now() / 1000)
   ) {
@@ -64,17 +52,10 @@ export const loader: LoaderFunction = async ({ request }) => {
       },
     });
 
-    /* if (requestType !== "getWCLReportQuery") { */
-    client.setHeader("Vercel-CDN-Cache-Control", "max-age=3600");
-    client.setHeader("CDN-Cache-Control", "max-age=60");
-    client.setHeader("Cache-Control", "max-age=100");
-    /* } */
-
     const data = await client.request(
       Queries[requestType as keyof QueryTypes],
       parsedVariables
     );
-    /* CACHE.set(cacheKey, data); */
 
     return data;
   } catch (error) {
