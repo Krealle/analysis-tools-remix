e8318f9892a3a39469f006a589f3a32a4060f8fc
Vollmer <kristianvollmer@gmail.com>
2024-04-28 15:04:43 +0200

    change credentials

--- app/routes/api.fightData.tsx
+++ app/routes/api.fightData.tsx
@@ -2,16 +2,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { LoaderFunction, json } from "@remix-run/node";
-import {
-  EventVariables,
-  fetchReportData,
-  getEvents,
-  getSummaryTable,
-} from "../wcl/util/queryWCL";
-import { getFilter } from "../wcl/util/filters";
-import { SummaryTableResponse } from "../wcl/types/graphql/queryTypes";
+import { EventVariables } from "../wcl/util/queryWCL";
 import { AccessSession, getSession } from "./sessions";
-/* import { ReportParseError } from "../wcl/util/parseWCLUrl"; */
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
@@ -32,39 +24,21 @@ export const loader: LoaderFunction = async ({ request }) => {
   }
 
   const parsedVariables = JSON.parse(variables) as EventVariables;
-  /* const parsedPhaseEventVariables = JSON.parse(
-    phaseEventVariables
-  ) as EventVariables; */
 
   try {
-    /* const summaryTable = await getSummaryTable(parsedVariables); */
-    /* const summaryTable = await fetchReportData<SummaryTableResponse>(
-      "summaryTable",
-      parsedVariables
-    ); */
-
     const queryParams = new URLSearchParams({
       requestType: "getSummaryTableQuery",
       variables: JSON.stringify(parsedVariables),
       session: JSON.stringify(accessSession),
     });
-    //const baseUrl = process.env.BASE_URL;
     const baseUrl = url.origin;
 
     console.info(`${baseUrl}/api/graphqlClient?`);
 
     const response = await fetch(
       `${baseUrl}/api/graphqlClient?` + queryParams.toString(),
-      { credentials: "same-origin" }
+      { credentials: "include" }
     );
-
-    /* console.log("");
-    console.log(
-      `${response.status} - ${response.statusText} - ${response.type} - ${response.url} `
-    );
-    console.log(response.headers);
-    console.log(response.body);
-    console.log(""); */
 
     if (response.ok) {
       const data = await response.json();
@@ -84,22 +58,8 @@ export const loader: LoaderFunction = async ({ request }) => {
       statusText: response.statusText,
       uri: `${baseUrl}/api/graphqlClient?`,
     };
-
-    /* parsedVariables.filterExpression = getFilter();
-    const events = await getEvents(parsedVariables);
-    const phaseEvents = await getEvents(parsedPhaseEventVariables); */
-
-    /* return {
-      summaryTable: summaryTable.table.data,
-      events: events,
-      phaseEvents: phaseEvents,
-    }; */
   } catch (error) {
     console.error((error as Error).toString());
-
-    return error; /* {
-      error: error as ReportParseError,
-      fight: parsedVariables?.fightIDs?.[0] || -1,
-    }; */
+    return error;
   }
 };
